b46ab4b342a4c4ca060fb325dc0c63748183cbfb
Kendrick <kendricknghy@gmail.com>
2023-07-18 12:53:55 +0800

    Added new file

---
+++ part1_functions.py
@@ -0,0 +1,9 @@
+# Imports from another file
+from fixed_parameters import TAGS
+from fixed_parameters import ES_train, RU_train
+from fixed_parameters import ES_dev_out, RU_dev_out
+
+# Functions
+
+#____________________TESTING____________________#
+# run funtions below
