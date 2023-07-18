31cba0fbe9608eb8a81ab68f4738d1ea9f8a5ef6
Kendrick <94118783+NinjaKen99@users.noreply.github.com>
2023-07-18 11:49:30 +0800

    Add files via upload

---
+++ fixed_parameters.py
@@ -0,0 +1,17 @@
+##### List of parameters used that are not subject to change #####
+
+### TAGS ###
+NUMBER_OF_TAGS = 7
+
+B_Positive = "B-positive"
+B_Negative = "B-negative"
+B_Neutral = "B-neutral"
+I_Positive = "I-positive"
+I_Negative = "I-negative"
+I_Neutral = "I-neutral"
+Outside = "O"
+
+TAGS = (B_Positive, B_Negative, B_Neutral, I_Positive, I_Negative, I_Neutral, Outside)
+
+### WORDS ###
+Invalid_Word = "#UNK#"
