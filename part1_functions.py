f2416a8d98c891bcc85993fdd6767625000bc4a3
Kendrick <kendricknghy@gmail.com>
2023-07-18 13:37:48 +0800

    part c in progress

--- part1_functions.py
+++ part1_functions.py
@@ -1,11 +1,12 @@
 # Imports from another file
 from fixed_parameters import TAGS
 from fixed_parameters import ES_train, RU_train
+from fixed_parameters import ES_dev_in, RU_dev_in
 from fixed_parameters import ES_dev_out, RU_dev_out
 from fixed_parameters import Invalid_Word as unknown
 
 # Functions
-def estimate_emission_parameter_v1(data, WORD, TAG):
+def estimate_emission_parameter_v1(data, WORD, TAG): # part a
     dataset = data.split("\n")
     counter = {"Count": 0, "Word": 0}
     for line in dataset:
@@ -18,7 +19,7 @@ def estimate_emission_parameter_v1(data, WORD, TAG):
     result = counter["Word"]/counter["Count"]
     return result
 
-def estimate_emission_parameter_v2(data, WORD, TAG):
+def estimate_emission_parameter_v2(data, WORD, TAG): # part b
     dataset = data.split("\n")
     counter = {"Count": 0, "Word": 0, "Unknown": 1}
     for line in dataset:
@@ -33,6 +34,20 @@ def estimate_emission_parameter_v2(data, WORD, TAG):
     if (WORD == unknown):
         return counter["Unknown"] / (counter["Count"] + counter["Unknown"])
     return counter["Word"] / (counter["Count"] + counter["Unknown"])
+
+def estimate_emission_parameter_v3(data, TAG): # modified for part c
+    dataset = data.split("\n")
+    counter = {"Count": 0, "Unknown": 1}
+    for line in dataset:
+        pair = line.split(" ")
+        word, tag = pair[0], pair[1]
+        if (tag == TAG):
+            counter["Count"] += 1
+            if (word == unknown):
+                counter["Unknown"] += 1
+            else:
+                counter[word] += 1
+    return counter
 
 #____________________TESTING____________________#
 # run funtions below
