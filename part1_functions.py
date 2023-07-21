6390e7f19602ecedfac16d955d4eb8c5076ae5f7
Kendrick <kendricknghy@gmail.com>
2023-07-21 15:54:50 +0800

    Fixed some dictionary stuff

--- part1_functions.py
+++ part1_functions.py
@@ -46,14 +46,14 @@ def estimate_emission_parameter_v3(data, TAG): # modified for part c
             if (word == unknown):
                 counter["Unknown"] += 1
             else:
-                if (word not in counter.keys):
+                if (word not in counter.keys()):
                     counter[word] = 1
                 else: counter[word] += 1
     # new dictionary with emission parameter for each word that exists
     emission_parameters = {"Unknown": counter["Unknown"]/(counter["Count"] + counter["Unknown"])}
     total = counter["Count"]
     k = counter["Unknown"]
-    for keys, values in counter.items:
+    for keys, values in counter.items():
         if (keys != "Count" or keys != "Unknown"):
             emission_parameters[keys] = counter[keys] / (counter["Count"] + counter["Unknown"])        
     return emission_parameters # return dictionary with all emission parameters
@@ -63,9 +63,9 @@ def produce_tag(data, TAGS):
     word_dict = {}
     for tag in TAGS:
         tag_dict[tag] = estimate_emission_parameter_v3(data, tag)
-        for key in tag_dict[tag].keys:
+        for key in tag_dict[tag].keys():
             word_dict[key] = None
-    for word in word_dict.keys:
+    for word in word_dict.keys():
         emission_parameter = 0
         y_star = None
         for tag in TAGS:
@@ -79,3 +79,5 @@ def produce_tag(data, TAGS):
 
 #____________________TESTING____________________#
 # run funtions below
+
+print(produce_tag(RU_train, TAGS))
